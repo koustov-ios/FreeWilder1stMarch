6a765049d99c7bc0bd152942655920277d2f067b
esolz1 <KoustovBasu@esolz1s-macbook-air.local>
2016-03-01 12:17:21 +0530

    Initial Commit

---
+++ FreeWilder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FreeWilder
+//
+//  Created by Koustov Basu on 01/03/16.
+//  Copyright © 2016 Koustov Basu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FreeWilderTests/FreeWilderTests.m
@@ -0,0 +1,39 @@
+//
+//  FreeWilderTests.m
+//  FreeWilderTests
+//
+//  Created by Koustov Basu on 01/03/16.
+//  Copyright © 2016 Koustov Basu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FreeWilderTests : XCTestCase
+
+@end
+
+@implementation FreeWilderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
